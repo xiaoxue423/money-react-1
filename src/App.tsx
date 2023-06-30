304ee0c90c003b01f38b4e0ddeaa8ddd1ce801ee
Nora.Hu <nora.h@thinkwithu.com>
2023-06-30 20:52:06 +0800

    Money.tsx 之 TagsSection UI完成

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,26 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Tag from "views/Tag";
 import Money from "views/Money";
 import Statistics from "views/Statistics";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+  color: #333;
+`;
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/tags" element={<Tag />} />
-        <Route path="/money" element={<Money />} />
-        <Route path="/statistics" element={<Statistics />} />
-        {/* <Route path="/" render={() => <Navigate to="about-us" />} /> */}
-        {/* <Redirect exact from="/" to="/money" /> */}
-        <Route path="/*" element={<NoMatch />} />
-      </Routes>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Routes>
+          <Route path="/tags" element={<Tag />} />
+          <Route path="/money" element={<Money />} />
+          <Route path="/statistics" element={<Statistics />} />
+          {/* <Route path="/" render={() => <Navigate to="about-us" />} /> */}
+          {/* <Redirect exact from="/" to="/money" /> */}
+          <Route path="/*" element={<NoMatch />} />
+        </Routes>
+      </Router>
+    </AppWrapper>
   );
 }
 
