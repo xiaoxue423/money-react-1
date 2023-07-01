92dcc39090ba06743a64813de450084a1065aa9b
Nora.Hu <nora.h@thinkwithu.com>
2023-07-01 09:41:51 +0800

    NumberPad Section

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -28,7 +28,6 @@ const TagsSection = styled.section`
 
 const NotesSection = styled.section`
   background-color: #f5f5f5;
-  border: 1px solid green;
   padding: 0 16px;
   font-size: 14px;
   > label {
@@ -58,13 +57,13 @@ const TypesSection = styled.section`
       text-align: center;
       padding: 16px 0;
       position: relative;
-      &.selected::after{
-        content: '';
+      &.selected::after {
+        content: "";
         display: block;
         height: 3px;
         background: #333;
         position: absolute;
-        bottom:0;
+        bottom: 0;
         left: 0;
         width: 100%;
       }
@@ -72,7 +71,63 @@ const TypesSection = styled.section`
   }
 `;
 
-const NumberPadSection = styled.section``;
+const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  > .output {
+    background: #fff;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+      inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+  }
+  > .pad {
+    > button {
+      font-size: 18px;
+      width: 25%;
+      height: 64px;
+      float: left;
+      border: none;
+      &.ok {
+        height: 128px;
+        float: right;
+      }
+      &.zero {
+        width: 50%;
+      }
+      &:nth-child(1) {
+        background: red;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: green;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: pink;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10){
+        background: yellow;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: gray;
+      }
+      &:nth-child(12) {
+        background: green;
+      }
+      &:nth-child(14) {
+        background: red;
+      }
+    }
+  }
+`;
 function Money() {
   return (
     <Layout>
@@ -98,25 +153,22 @@ function Money() {
         </ul>
       </TypesSection>
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className="output">100</div>
+        <div className="pad clearfix">
           <button>1</button>
           <button>2</button>
           <button>3</button>
-          <button>+</button>
           <button>删除</button>
           <button>4</button>
-          <button>1</button>
-          <button>1</button>
-          <button>1</button>
-          <button>1</button>
-          <button>1</button>
-          <button>1</button>
-          <button>1</button>
-          <button>1</button>
-          <button>1</button>
-          <button>1</button>
-          <button>1</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
         </div>
       </NumberPadSection>
     </Layout>
