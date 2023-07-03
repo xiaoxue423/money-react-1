e16b198e655d7c2984e898cd733cd8b6e6222828
Nora.Hu <nora.h@thinkwithu.com>
2023-07-03 18:02:18 +0800

    useParams与findTag

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -7,13 +7,22 @@ const defaultTags = [
   { id: createId(), name: "住" },
   { id: createId(), name: "行" },
 ];
+interface Tag {
+  id: number;
+  name: string;
+}
 const useTags = () => {
   // 封装一个自定义Hook
-  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
+  const [tags, setTags] = useState(defaultTags);
+
+  const findTag = (id: number) => {
+    return tags.filter((tag) => tag.id === id)[0];
+  };
 
   return {
     tags,
     setTags,
+    findTag,
   };
 };
 
--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,7 +1,22 @@
+import Layout from "components/Layout";
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useTags } from "useTags";
 
+type Params = {
+  id: string;
+};
 const Tag: React.FC = () => {
-  return <div>hi</div>;
+  const { findTag } = useTags();
+
+  const { id } = useParams<Params>();
+
+  const tag = id !== undefined ? findTag(parseInt(id)) : "";
+  return (
+    <Layout>
+      <div>{tag === "" ? "" : tag.name}</div>
+    </Layout>
+  );
 };
 
 export { Tag };
