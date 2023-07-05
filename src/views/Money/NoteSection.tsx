e69de7b5fb9eed61f0e1346a4fbc3c0c9b167e68
Nora.Hu <nora.h@thinkwithu.com>
2023-07-05 22:44:07 +0800

    创建单独的Input

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,51 +1,30 @@
-import { useState, useRef } from "react";
+import { Input } from "components/Input";
+import { useState, useRef, ChangeEventHandler } from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.section`
   background-color: #f5f5f5;
   padding: 0 16px;
   font-size: 14px;
-  > label {
-    display: flex;
-    align-items: center;
-    > span {
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    > input {
-      display: block;
-      width: 100%;
-      height: 72px;
-      border: none;
-      background: none;
-    }
-  }
 `;
 
 type Props = { value: string; onChange: (v: string) => void };
 
 const NoteSection: React.FC<Props> = (props) => {
   // const [note, setNote] = useState("");
-  const note = props.value
-  const inputRef = useRef<HTMLInputElement>(null);
-  const onBlur = () => {
-    if (inputRef.current !== null) {
-      console.log("note", inputRef.current.value);
-      props.onChange(inputRef.current.value);
-    }
+  const note = props.value;
+  const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    props.onChange(e.target.value);
   };
   return (
     <Wrapper>
-      <label>
-        <span>备注</span>
-        <input
-          ref={inputRef}
-          type="text"
-          placeholder="在这里添加备注"
-          defaultValue={note}
-          onBlur={onBlur}
-        />
-      </label>
+      <Input
+        type="text"
+        label="备注"
+        placeholder="在这里添加备注"
+        value={note}
+        onChange={onChange}
+      />
     </Wrapper>
   );
 };
